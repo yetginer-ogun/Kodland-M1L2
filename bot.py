e0c56d9878e4bbcf568403ae307115cd1ed1d0c0
Mehmet Ogün Yetginer <yetginer.mehmetogun@gmail.com>
2024-06-04 15:40:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,8 +12,7 @@ async def on_ready():
     print(f'{bot.user} olarak giriş yaptık')
 
 @bot.command()
-async def temizlik(ctx):
-    await ctx.send(cumleler.atik())
-
+async def selam(ctx):
+    await ctx.send(f"Selam {ctx.author}. Ben {bot.user}")
 
 bot.run()
